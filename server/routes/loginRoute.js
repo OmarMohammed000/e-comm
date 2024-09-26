e5fc18a1830711c6875a32cfc4bd2ab30217686e
omar <omarmohammedelsayed00>
2024-09-26 16:29:50 +0300

    adding in geter for the user

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import loginApp from "../controllers/login.js";
+import getUserByJwt from "../controllers/getUserByJWT.js";
 const router=express.Router();
 // login route
-
+router.get('/user',getUserByJwt);
 router.post('/login',loginApp);
 export default router
