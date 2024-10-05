63449db322df870371c04883d2be28560c8cd3cd
omar <omarmohammedelsayed00>
2024-10-05 19:21:02 +0300

    fixing port issuie in render

--- server/index.js
+++ server/index.js
@@ -61,7 +61,7 @@ db.sequelize
   .sync({ force: false, alter: true })
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
