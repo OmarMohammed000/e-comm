e7e853583cec02848c740da5ce484e8d02b88cfe
omar <omarmohammedelsayed00>
2024-10-06 14:12:34 +0300

    adjasting session settings

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure:true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
