57fd9f73427ec241b5de6f001a0954737922d7c9
omar <omarmohammedelsayed00>
2024-10-06 16:55:38 +0300

    trying different samesite setting

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure:  process.env.NODE_ENV === 'production' ,
-      sameSite: "none",
+      sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
